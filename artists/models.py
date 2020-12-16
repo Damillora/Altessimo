d91e794eec7a6b4cb58b18c2a29a7fcc2f70e781
Damillora <developer@damillora.com>
2020-12-16 14:29:08 +0700

    Modify some more

--- artists/models.py
+++ artists/models.py
@@ -2,14 +2,31 @@ from django.db import models
 from django.apps import apps
 
 # Create your models here.
+class ArtistManager(models.Manager):
+    def create_or_new(self, romanized_name):
+        romanized_name = romanized_name.strip()
+        qs = self.get_queryset().filter(romanized_name__iexact=romanized_name)
+        if qs.exists():
+            return qs.first(), False
+        return Artist.objects.create(romanized_name=romanized_name), True
+    
+    def comma_to_qs(self, artists_str):
+        final_ids = []
+        for artist in artists_str.split(','):
+            obj, created = self.create_or_new(artist)
+            final_ids.append(obj.id)
+        qs = self.get_queryset().filter(id__in=final_ids).distinct()
+        return qs
 
 class Artist(models.Model):
-    name = models.CharField(max_length=255)
-    romanized_name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255,blank=True)
+    romanized_name = models.CharField(max_length=255,blank=True)
     aliases = models.ManyToManyField("self",blank=True)
     category = models.ManyToManyField("categories.Category",blank=True)
     about_composer = models.TextField(blank=True)
     about_music = models.TextField(blank=True)
+
+    objects = ArtistManager()
     
     def __str__(self):
         return self.romanized_name
--- categories/models.py
+++ categories/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class CategoryManager(models.Manager):
+    def create_or_new(self, name):
+        name = name.strip()
+        qs = self.get_queryset().filter(name__iexact=name)
+        if qs.exists():
+            return qs.first(), False
+        return Category.objects.create(name=name), True
+    
+    def comma_to_qs(self, categorys_str):
+        final_ids = []
+        for category in categories_str.split(','):
+            obj, created = self.create_or_new(category)
+            final_ids.append(obj.id)
+        qs = self.get_queryset().filter(id__in=final_ids).distinct()
+        return qs
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
+
+    objects = CategoryManager()
 
     class Meta:
         verbose_name_plural = "Categories"
