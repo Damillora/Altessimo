b1f9605fab7e162dac2ce2d01af9349e2f3f2782
Damillora <developer@damillora.com>
2020-12-17 14:12:26 +0700

    Add colors and alias handling

--- artists/views.py
+++ artists/views.py
@@ -10,6 +10,12 @@ def artist_index(request):
 
 def artist_show(request, slug):
     artist = Artist.objects.filter(slug=slug)[0]
-    credit_songs = (artist.written_songs.all() | artist.composed_songs.all() | artist.arranged_songs.all()).distinct()
+    credit_songs = artist.written_songs.all() | artist.composed_songs.all() | artist.arranged_songs.all()
+    aliases = artist.aliases.all()
     outside_songs = OutsideSong.objects.filter(composer=artist)
+    for alias in aliases:
+        credit_songs = credit_songs | alias.written_songs.all() | artist.composed_songs.all() | artist.arranged_songs.all()
+        outside_songs = outside_songs | OutsideSong.objects.filter(composer=alias)
+    credit_songs = credit_songs.distinct()
+    outside_songs = outside_songs.distinct()
     return render(request,'artists/show.html',{'artist': artist,'credit_songs':credit_songs,'outside_songs':outside_songs})
