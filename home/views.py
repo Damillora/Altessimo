01d2c14e270c285ebdcf9285c896a506c8dfd054
Damillora <damillora@damillora.com>
2021-07-10 20:41:17 +0700

    Rework randomizer interface

--- home/views.py
+++ home/views.py
@@ -5,17 +5,23 @@ from songs.models import Song
 from idols.models import Idol
 
 # Create your views here.
+
+
 def index(request):
-    return render(request,"index.html")
+    return render(request, "index.html")
+
 
 def randomizer(request):
     obj = {}
-    if "num" in request.GET:
-        obj['num'] = request.GET['num']
-        song_ids = list(Song.objects.values_list('pk',flat=True))
+    if "song_id" in request.GET:
+        obj['song'] = Song.objects.get(pk=request.GET['song_id'])
+    else:
+        song_ids = list(Song.objects.values_list('pk', flat=True))
         song_id = random.choice(song_ids)
         obj['song'] = Song.objects.get(pk=song_id)
-        idol_ids = list(Idol.objects.values_list('pk',flat=True))
-        idol_selected_ids = random.sample(idol_ids,int(request.GET['num']))
+    if "num" in request.GET:
+        obj['num'] = request.GET['num']
+        idol_ids = list(Idol.objects.values_list('pk', flat=True))
+        idol_selected_ids = random.sample(idol_ids, int(request.GET['num']))
         obj['idols'] = Idol.objects.filter(pk__in=idol_selected_ids)
-    return render(request,"randomizer.html",obj)
+    return render(request, "randomizer.html", obj)
